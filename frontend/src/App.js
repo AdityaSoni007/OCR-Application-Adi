f49f12af4d75e99bb479a22a260e1d929f2e5418
Aditya Soni <soniaditya20032002@gmail.com>
2023-12-24 13:28:46 +0530

    Set up a Tailind CSS environment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
-      
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+
     </>
   );
 }
